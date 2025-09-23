633261588e05f9b3b1729d2050f6de62541ecd28
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-09-23 11:29:30 -0600

    update of the findone service for a better performance

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '../../generated/prisma';
@@ -49,7 +49,10 @@ export class ProductService extends PrismaClient implements OnModuleInit{
     
 
     if ( !product ) {
-      throw new RpcException(`Product with id ${id} not found`);
+      throw new RpcException({
+        message: `Product with id ${id} not found`,
+        status: HttpStatus.BAD_REQUEST
+      });
     }
 
     return product;
